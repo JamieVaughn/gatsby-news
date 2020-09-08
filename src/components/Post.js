4e97c011a907110a2d74ffeabd5df882951c14d0
JamieVaughn <wxv6@case.edu>
2020-09-08 00:54:48 -0400

    Add thumbnail images to posts list items

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import {Link} from 'gatsby'
 import {Card, CardBody, CardTitle, CardSubtitle, CardText} from 'reactstrap'
+import Img from 'gatsby-image'
 
 
 const Post = (props) => {
     console.log(props)
-    const {title, author, path, date} = props.node.frontmatter
+    const {title, author, path, date, image} = props.node.frontmatter
     const excerpt = props.node.excerpt
+    const fluid = image.childImageSharp.fluid
     return (
         <Card>
+            <Link to={path}>
+                <Img className='card-image-top' fluid={fluid} />
+            </Link>
             <CardBody>
                 <CardTitle>
                     <Link to={path}>{title}</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import SEO from "../components/seo"
 import {graphql, StaticQuery} from 'gatsby'
 import Post from '../components/Post'
 import { node } from "prop-types"
+import { Row, Col} from 'reactstrap'
 
 const IndexQuery = graphql`
   query {
@@ -17,6 +18,13 @@ const IndexQuery = graphql`
             date(formatString: "MM DD YYYY")
             author
             path
+            image {
+              childImageSharp {
+                fluid(maxWidth: 600) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
           excerpt
         }
@@ -29,15 +37,22 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['MMT', 'Monetary', 'Money', 'Cricut', 'Investing', 'Economics', 'Modern Monetary Theory', 'Money Circuit']}/>
     <h1>Home Page</h1>
-    <StaticQuery query={IndexQuery} render={data=> {
-      return (
-        <div>
-          {data.allMarkdownRemark.edges.map((data) => (
-            <Post key={data.node.id} node={data.node}/>
-          ))}
-        </div>
-      )
-    }} />
+    <Row>
+      <Col xs="12" md="9">
+      <StaticQuery query={IndexQuery} render={data=> {
+        return (
+          <div>
+            {data.allMarkdownRemark.edges.map((data) => (
+              <Post key={data.node.id} node={data.node}/>
+            ))}
+          </div>
+        )
+      }} />
+      </Col>
+      <Col md="3" style={{background: '#ddd'}}>
+        Sidebar
+      </Col>
+    </Row>
   </Layout>
 )
 
